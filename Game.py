906d4c0bde2daba0770dcc8c96089b7a9928bf9a
Uliankin228 <uiv2050@yandex.ru>
2020-12-23 20:21:58 +0300

    Добавил спрайты игрока и платформы, пока никак не реагируют друг с другом, игрок может передвигаться и падать

--- Game.py
+++ Game.py
@@ -29,11 +29,46 @@ class Game:
                 self.running = False
 
     def update(self):  # Обнавление
-        pass
+        character_sprites.update()
 
     def visualization(self):  # Визуализация
         self.screen.fill('black')
+        character_sprites.draw(self.screen)
+        platform_sprites.draw(self.screen)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, group):
+        super().__init__(group)
+        self.image = pygame.Surface((50, 100))
+        self.image.fill('green')
+        self.rect = self.image.get_rect()
+        self.rect.center = WIDTH // 2, HEIGHT // 2
+        self.fall = 5
+
+    def update(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a] and self.rect.left > 5:
+            self.rect.x -= 5
+        if keys[pygame.K_d] and self.rect.right < WIDTH - 5:
+            self.rect.x += 5
+
+        self.rect.y += self.fall  # Падение
+        if self.rect.bottom > HEIGHT - 5:
+            self.rect.bottom = HEIGHT - 5
+
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, coords):
+        super().__init__()
+        self.image = pygame.Surface((210, 20))
+        self.image.fill('blue')
+        self.rect = self.image.get_rect()
+        self.rect.center = coords
 
 
 if __name__ == '__main__':
+    character_sprites, platform_sprites = pygame.sprite.Group(), pygame.sprite.Group()
+    player = Player(character_sprites)
+    platform_sprites.add(Platform((100, 600)))
     game = Game()
