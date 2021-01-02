67daa7b8a23a7b6c46b8f7b2c4b04ee40d98fad5
Uliankin228 <uiv2050@yandex.ru>
2021-01-02 13:06:38 +0300

    Добавил графику, есть анимация хождения у врагов

--- Game.py
+++ Game.py
@@ -11,6 +11,7 @@ pygame.init()
 class Game:
     def __init__(self):
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.bg = load_image('background.jpg')
         self.running = True
         self.game_run()
 
@@ -32,11 +33,13 @@ class Game:
         character_sprites.update()
 
     def visualization(self):  # Визуализация
-        self.screen.fill('black')
+        self.screen.blit(self.bg, [0, 0])
         character_sprites.draw(self.screen)
         platform_sprites.draw(self.screen)
 
-        visual_board.render(self.screen)
+        # visual_board.render(self.screen)
+
+        pygame.draw.rect(self.screen, 'red', [25, 15, player.hp // 2, 25], 0)
 
 
 class VisualBoard:
@@ -63,11 +66,10 @@ class VisualBoard:
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(character_sprites)
-        self.image = pygame.Surface((33, 58))
-        self.image.fill('green')
+        self.image = load_image('Player.png', (35, 90, 115))
         self.rect = self.image.get_rect()
         self.rect.center = WIDTH // 2, HEIGHT // 2
-        self.fall = 5
+        self.fall, self.hp, self.inwis = 5, 1000, 0
         self.jump, self.is_flight = [False, -25], False
 
     def update(self):
@@ -107,6 +109,13 @@ class Player(pygame.sprite.Sprite):
             self.is_flight = False
         if self.jump[0]:
             self.is_flight = True
+        hits = pygame.sprite.spritecollide(self, character_sprites, False)
+        for chr in hits:
+            if chr != self and self.inwis < 10:
+                self.hp -= 10
+                self.inwis += 5
+        if self.inwis > 0:
+            self.inwis -= 1
 
     def collision(self):
         platforms = pygame.sprite.spritecollide(self, platform_sprites, False)
@@ -131,22 +140,35 @@ class Player(pygame.sprite.Sprite):
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(character_sprites)
-        self.image = pygame.Surface((33, 58))
-        self.image.fill('red')
+        self.image_stand = load_image('Guard.png', (35, 90, 115))
+        self.image = self.image_stand
+        self.frames_run_right = load_frames(load_image('Guard_run.png', (35, 90, 115)), 6, 1)
+        self.frames_run_left = []
+        for im in self.frames_run_right:
+            image = pygame.transform.flip(im, True, False)
+            image.set_colorkey((35, 90, 115))
+            self.frames_run_left.append(image)
+        self.cur_frame = 0
         self.rect = self.image.get_rect()
         self.rect.center = WIDTH // 2, HEIGHT - 23
-        self.fall, self.speed = 5, random.randrange(4, 7)
+        self.fall, self.speed = 5, random.randrange(4, 6)
         self.jump, self.is_flight = [False, -25], False
         self.path, self.last_player_cell_coord = [], []
-        self.run, self.timers = [0, 1, False], [30]
+        self.run, self.timers = [0, 1, False], [30, 0]
 
     def update(self):
         cell_player, cell_self = visual_board.get_coords(player.rect.center), visual_board.get_coords(self.rect.center)
         if cell_player[1] >= cell_self[1] and not self.jump[0]:
             if player.rect.right < self.rect.left and self.rect.left > 5:
                 self.rect.x -= self.speed
+                if self.timers[1] == 3:
+                    self.cur_frame = (self.cur_frame + 1) % len(self.frames_run_right)
+                    self.image = self.frames_run_left[self.cur_frame]
             if player.rect.left > self.rect.right and self.rect.right < WIDTH - 5:
                 self.rect.x += self.speed
+                if self.timers[1] == 3:
+                    self.cur_frame = (self.cur_frame + 1) % len(self.frames_run_right)
+                    self.image = self.frames_run_right[self.cur_frame]
         elif self.path:
             if self.path[0][0] > cell_self[0] and self.run[0] != self.path[0][0] * 100 + 50:
                 self.run = [self.path[0][0] * 100 + 50, 1, False]
@@ -170,6 +192,14 @@ class Enemy(pygame.sprite.Sprite):
                     self.run[0] = 0
             elif self.run[0] == 0 and not self.jump[0]:
                 del self.path[0]
+            if self.run[0] != 0 and self.timers[1] == 3:
+                self.cur_frame = (self.cur_frame + 1) % len(self.frames_run_right)
+                if self.run[1] == -1:
+                    self.image = self.frames_run_left[self.cur_frame]
+                else:
+                    self.image = self.frames_run_right[self.cur_frame]
+        else:
+            self.image = self.image_stand
         if self.jump[0]:
             if self.jump[1] == 25:
                 self.jump = [False, -25]
@@ -202,6 +232,10 @@ class Enemy(pygame.sprite.Sprite):
             self.last_player_cell_coord, self.timers[0] = cell_player, 30
             self.finding_path_1(visual_board.board, cell_player, cell_self)
         self.timers[0] -= 1
+        if self.timers[1] == 3:
+            self.timers[1] = 0
+        else:
+            self.timers[1] += 1
 
     def collision(self):
         platforms = pygame.sprite.spritecollide(self, platform_sprites, False)
@@ -236,7 +270,7 @@ class Enemy(pygame.sprite.Sprite):
             board2[cell_self[1]][cell_self[0]] = [1, '1']
             sp = self.finding_path_2(board2, cell_player)
             if sp[0]:
-                self.save_path(sp[1], cell_player, cell_self)
+                self.save_path(sp[1], cell_player)
 
     def finding_path_2(self, board, cell_player):
         wave = 1
@@ -297,7 +331,7 @@ class Enemy(pygame.sprite.Sprite):
                             return [True, board]
         return [False, []]
 
-    def save_path(self, board, cell_player, cell_self):
+    def save_path(self, board, cell_player):
         wave, cod_cell = board[cell_player[1]][cell_player[0]][0], board[cell_player[1]][cell_player[0]][1]
         self.path.append([*cell_player, False])
         for ind in range(-1, -len(cod_cell), -1):
@@ -305,7 +339,7 @@ class Enemy(pygame.sprite.Sprite):
             cod = cod_cell[:ind]
             for y in range(visual_board.height):
                 for x in range(visual_board.width):
-                    if board[y][x] == [wave, cod] and [x, y] != cell_self:
+                    if board[y][x] == [wave, cod]:
                         self.path.append([x, y, False])
                         if self.path[-2][1] < y:
                             self.path[-2][2] = True
@@ -313,22 +347,42 @@ class Enemy(pygame.sprite.Sprite):
 
 
 class Platform(pygame.sprite.Sprite):
-    def __init__(self, coords_cell):
+    def __init__(self, coord_cell):
         super().__init__()
-        self.image = pygame.Surface((100, 20))
-        self.image.fill('blue')
+        self.image = load_image('platform.png')
         self.rect = self.image.get_rect()
-        visual_board.set_values(coords_cell, 1)
-        self.rect.center = visual_board.get_coords_platform(coords_cell)
+        visual_board.set_values(coord_cell, 1)
+        self.rect.center = visual_board.get_coords_platform(coord_cell)
+
+
+def load_frames(image, columns, rows):
+    rect = pygame.Rect(0, 0, image.get_width() // columns, image.get_height() // rows)
+    frames = []
+    for j in range(rows):
+        for i in range(columns):
+            frame_location = (rect.w * i, rect.h * j)
+            frames.append(image.subsurface(pygame.Rect(frame_location, rect.size)))
+    return frames
+
+
+def load_image(name, color_key=None):
+    try:
+        image = pygame.image.load(os.path.join('image', name))
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+    if color_key is not None:
+        image.set_colorkey(color_key)
+    return image
 
 
 if __name__ == '__main__':
     visual_board = VisualBoard()
     character_sprites, platform_sprites = pygame.sprite.Group(), pygame.sprite.Group()
     player = Player()
-    for _ in range(4):
+    for _ in range(5):
         Enemy()
-    platform_sprites.add(Platform((2, 5)), Platform((3, 5)), Platform((4, 5)),
-                         Platform((6, 4)), Platform((7, 4)), Platform((8, 2)))
+    platform_sprites.add(Platform((2, 5)), Platform((3, 5)), Platform((4, 5)), Platform((6, 4)), Platform((7, 4)),
+                         Platform((8, 2)))
     game = Game()
 
