875c08ad4514749cb1f0aaf3754dded27839b901
1Yaroslav11-sh <yaroslavshavrov@yandex.ru>
2021-01-10 14:20:53 +0300

    Добавление фона

--- Menu2.py
+++ Menu2.py
@@ -1,8 +1,32 @@
 import pygame
+import os
 
 size = width, height = 1270, 750
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Меню')
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        image = image.convert()
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_color_key(color_key)
+    else:
+        image = image.convert_alpha()
+
+    return image
+
+
+image1 = load_image("FON1.jpg")  # загрузка фона
+image11 = pygame.transform.scale(image1, (1280, 750))
 
 if __name__ == '__main__':
     running = True
@@ -10,5 +34,5 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill('black')
+        screen.blit(image11, (0, 0))
         pygame.display.flip()
